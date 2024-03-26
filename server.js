cd51f5cd1403b7270d035263bdd23f37fdf54389
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-03-26 11:44:36 +0500

    Add server.js file with initial setup

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import connection from "./src/database/connection.js";
+
+// Initialize express
+const app = express();
+
+// Middleware
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+//Database Connection
+connection();
+
+// Starting endpoint
+app.get("/", (_req, res) => {
+  res.send("<h1 style='color:green;'>Hurrah! Server is running.</h1>");
+});
+
+// Routes
+// app.use("/api/user", userRouter);
+// app.use(notFound);
+// app.use(errorHandler);
+
+// Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
