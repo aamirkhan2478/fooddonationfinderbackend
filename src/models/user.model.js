cc592d94ebc266ac540a93ba4c4467e4efc12888
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-03-27 12:35:32 +0500

    Add user model with encryption and token generation

---
+++ src/models/user.model.js
@@ -0,0 +1,57 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userType: {
+    type: String,
+    required: true,
+    enum: ["Admin", "Donor", "Recipient"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+//Encrypt Password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+// Generate JWT Token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "24h",
+  });
+};
+
+// Check Password Match
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = model("User", userSchema);
+
+export default User;
