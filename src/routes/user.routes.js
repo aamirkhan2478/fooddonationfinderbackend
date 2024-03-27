86e208752c88af60bfed7548447237c0041dc5e8
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-03-27 12:35:37 +0500

    Add register, verify-email, and login routes to user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
 const router = express.Router();
-// import { register, login, logout } from '../controllers/user.controller.js';
+import { register, login, verifyEmail } from '../controllers/user.controller.js';
+
+router.post('/register', register);
+router.post('/verify-email', verifyEmail);
+router.post('/login', login);
 
 export default router;
