82efe8a5821add6d321a3f0310189c36975cc3d2
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-03-28 04:58:13 +0500

    Add new routes and controllers for user registration, login, email verification, password reset, and user profile update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,23 @@
 import express from "express";
-const router = express.Router();
-import { register, login, verifyEmail } from '../controllers/user.controller.js';
+import {
+  register,
+  login,
+  verifyEmail,
+  forgotPassword,
+  getUser,
+  resetPassword,
+  updateUser,
+} from "../controllers/user.controller.js";
+import auth from "../middleware/auth.middleware.js";
 
-router.post('/register', register);
-router.post('/verify-email', verifyEmail);
-router.post('/login', login);
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/verify-email", verifyEmail);
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.get("/", auth, getUser);
+router.put("/", auth, updateUser);
 
 export default router;
