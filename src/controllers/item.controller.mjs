c22cd9933e1b11afdec0b0050bb19a6c75e89cd3
Numan Anwar <86041286+numananwar789@users.noreply.github.com>
2024-04-22 10:32:21 +0500

    added response message in updateItem function

--- src/controllers/item.controller.mjs
+++ src/controllers/item.controller.mjs
@@ -109,7 +109,7 @@ export const updateItem = async (req, res) => {
     );
 
     // Send the updated item as a response
-    return res.status(200).json(item);
+    return res.status(200).json({ success: true, message: "Item added successfully" });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
