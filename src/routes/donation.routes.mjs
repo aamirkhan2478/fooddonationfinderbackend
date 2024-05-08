a18103481f90576dbbaa0e5f8a3d9388421868e3
DarkenTech1234@ <aamir@darkentech.com>
2024-05-08 11:28:20 +0500

    Add countClaimedDonations function to donation controller

--- src/routes/donation.routes.mjs
+++ src/routes/donation.routes.mjs
@@ -8,6 +8,7 @@ import {
   claimDonation,
   countDonations,
   countDonorsRecipients,
+  countClaimedDonations
 } from "../controllers/donation.controller.mjs";
 
 const router = express.Router();
@@ -17,6 +18,7 @@ router.get("/all", getDonations);
 router.get("/:id/show", getDonation);
 router.get("/count", countDonations);
 router.get("/count/donors-recipients", countDonorsRecipients);
+router.get("/count/claimed-donations", countClaimedDonations);
 router.delete("/:id/delete", deleteDonation);
 router.patch("/:id/update", updateDonationStatus);
 router.patch("/:id/claim", claimDonation);
